55207da366615a9d8d4a7eafdaedb26eb91cfd7a
vaynetian <vaynetian@cc98.org>
2018-12-29 19:10:28 +0800

    init: 初始化项目

---
+++ src/@types/third.d.ts
@@ -0,0 +1,8 @@
+declare module 'rc-form'
+declare module 'random-hash'
+declare module 'react-awesome-button'
+
+declare module 'dayjs'
+declare module 'dayjs/locale/zh-cn'
+declare module 'dayjs/plugin/relativeTime'
+
